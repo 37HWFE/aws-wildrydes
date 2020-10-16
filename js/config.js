5b56b9d6041c2506cbe340a0412494ee6a08ac92
zhou guilin <>
2020-10-16 15:33:21 +0800

    提交demo项目

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-2_wxV1sXgOZ',
+        userPoolClientId: 'ml926e9q8d1qp9dpt52bp18sk',
+        region: 'ap-northeast-2'
+    },
+    api: {
+        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
